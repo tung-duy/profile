4d53823a4b1930dae55b87ae5ec78125735c6962
Tung Duy <daoduytung9x@gmail.com>
2018-10-09 21:01:57 +0700

    done profile validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load Profile Model
 const Profile = require("../../models/Profile");
 // Load User Model
@@ -23,6 +26,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -41,6 +45,12 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+    //Check validation
+    if (!isValid) {
+      // Return any errors
+      return res.status(400).json(errors);
+    }
     // GET fields
     const profileFields = {};
     profileFields.user = req.user.id;
@@ -52,6 +62,7 @@ router.post(
     if (req.body.status) profileFields.status = req.body.status;
     if (req.body.githubusername)
       profileFields.githubusername = req.body.githubusername;
+
     //Skills - Spilt into array
     if (typeof req.body.skills !== "undefined") {
       profileFields.skills = req.body.skills.split(",");
